a01b2199e4d2c44415d47805b2d6404775402f42
Andyckooo <andrejtoth2077@gmail.com>
2025-11-10 20:15:54 +0100

    Add smooth scrolling for hash links and logo click in Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Toaster } from './components/ui/sonner';
 import { HomePage } from './pages/HomePage';
 import { PricingPage } from './pages/PricingPage';
+import { useEffect } from 'react';
+
+function ScrollToHash() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      // Počkaj kým sa stránka načíta
+      setTimeout(() => {
+        const element = document.querySelector(location.hash);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, 100);
+    }
+  }, [location]);
+
+  return null;
+}
 
 export default function App() {
   return (
     <Router>
       <div className="min-h-screen">
+        <ScrollToHash />
         <Header />
         <main>
           <Routes>
